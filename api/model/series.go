f3aff8b5442290416eb0dae5181304f1fa24c17d
oneminuter <1050086935@qq.com>
2018-11-19 00:11:33 +0800

    故事相关结构体数据库操作统一封装

--- api/model/series.go
+++ api/model/series.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"time"
+	"wyatt/api/constant"
+	"wyatt/db"
+	"wyatt/util"
+)
+
 //系列故事，如小说之类的，有很多篇的，连续的故事
 type Series struct {
 	TableModel
@@ -12,4 +19,99 @@ type Series struct {
 	ViewedNum  int64  `json:"viewedNum"`  //浏览量, 此字段在故事章节里是每章节的总浏览量，在系列里是该系列所有故事的总浏览量
 	ZanNum     int64  `json:"zanNum"`     //点赞量，此字段在故事章节里是每章节的总点赞量，在系列里是该系列所有故事的总点赞量
 	CommentNum int64  `json:"commentNum"` //评论数量，此字段在故事章节里是每章节的总评论量，在系列里是该系列所有故事的总评论量
+}
+
+func (m *Series) BeforeCreate() (err error) {
+	m.FlowId = time.Now().Unix()
+	return
+}
+
+func (m *Series) Add() error {
+	mdb := db.GetMysqlDB()
+	tx := mdb.Begin()
+	defer tx.Commit()
+	err := tx.Create(m).Error
+	if err != nil {
+		util.LoggerError(err)
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func (m *Series) Delete(where interface{}, args ...interface{}) error {
+	mdb := db.GetMysqlDB()
+	tx := mdb.Begin()
+	defer tx.Commit()
+	err := tx.Model(m).Where(where, args...).Delete(m).Error
+	if err != nil {
+		util.LoggerError(err)
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func (m *Series) Update(update, where interface{}, args ...interface{}) error {
+	mdb := db.GetMysqlDB()
+	tx := mdb.Begin()
+	defer tx.Commit()
+	err := tx.Model(m).Where(where, args...).Updates(update).Error
+	if err != nil {
+		util.LoggerError(err)
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func (m *Series) QueryList(field string, page, limit int, where interface{}, args ...interface{}) ([]Series, error) {
+	if 0 > page {
+		page = 0
+	}
+	if 0 > limit || constant.MAX_QUERY_COUNT < limit {
+		limit = constant.MAX_QUERY_COUNT
+	}
+
+	mdb := db.GetMysqlDB()
+	var list = make([]Series, 0)
+	err := mdb.Model(m).Select(field).Where(where, args...).Offset(page * limit).Limit(limit).Find(&list).Error
+	if err != nil {
+		util.LoggerError(err)
+		return make([]Series, 0), err
+	}
+	return list, nil
+}
+
+func (m *Series) QueryOne(field string, where interface{}, args ...interface{}) error {
+	mdb := db.GetMysqlDB()
+	err := mdb.Model(m).Select(field).Where(where, args...).Last(m).Error
+	if err != nil {
+		util.LoggerError(err)
+		return err
+	}
+
+	return nil
+}
+
+func (m *Series) QueryCount(where interface{}, args ...interface{}) (int, error) {
+	mdb := db.GetMysqlDB()
+	var count int
+	err := mdb.Model(m).Where(where, args...).Count(&count).Error
+	if err != nil {
+		util.LoggerError(err)
+		return 0, err
+	}
+	return count, nil
+}
+
+func (m *Series) QueryGrounp(field string, group string, where interface{}, args ...interface{}) ([]Series, error) {
+	mdb := db.GetMysqlDB()
+	var list = make([]Series, 0)
+	err := mdb.Model(m).Select(field).Where(where, args...).Group(group).Find(&list).Error
+	if err != nil {
+		util.LoggerError(err)
+		return make([]Series, 0), err
+	}
+	return list, nil
 }
--- api/model/storyContent.go
+++ api/model/storyContent.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"time"
+	"wyatt/api/constant"
+	"wyatt/db"
+	"wyatt/util"
+)
+
 //故事的每条内容
 type StoryContent struct {
 	TableModel
@@ -8,4 +15,99 @@ type StoryContent struct {
 	Type    string `json:"type"`    //1 角色对白，2 旁白
 	RoleId  int64  `json:"roleId"`  //角色id, 如果是旁白，该字段为空
 	Context string `json:"context"` //内容
+}
+
+func (m *StoryContent) BeforeCreate() (err error) {
+	m.FlowId = time.Now().Unix()
+	return
+}
+
+func (m *StoryContent) Add() error {
+	mdb := db.GetMysqlDB()
+	tx := mdb.Begin()
+	defer tx.Commit()
+	err := tx.Create(m).Error
+	if err != nil {
+		util.LoggerError(err)
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func (m *StoryContent) Delete(where interface{}, args ...interface{}) error {
+	mdb := db.GetMysqlDB()
+	tx := mdb.Begin()
+	defer tx.Commit()
+	err := tx.Model(m).Where(where, args...).Delete(m).Error
+	if err != nil {
+		util.LoggerError(err)
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func (m *StoryContent) Update(update, where interface{}, args ...interface{}) error {
+	mdb := db.GetMysqlDB()
+	tx := mdb.Begin()
+	defer tx.Commit()
+	err := tx.Model(m).Where(where, args...).Updates(update).Error
+	if err != nil {
+		util.LoggerError(err)
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func (m *StoryContent) QueryList(field string, page, limit int, where interface{}, args ...interface{}) ([]StoryContent, error) {
+	if 0 > page {
+		page = 0
+	}
+	if 0 > limit || constant.MAX_QUERY_COUNT < limit {
+		limit = constant.MAX_QUERY_COUNT
+	}
+
+	mdb := db.GetMysqlDB()
+	var list = make([]StoryContent, 0)
+	err := mdb.Model(m).Select(field).Where(where, args...).Offset(page * limit).Limit(limit).Find(&list).Error
+	if err != nil {
+		util.LoggerError(err)
+		return make([]StoryContent, 0), err
+	}
+	return list, nil
+}
+
+func (m *StoryContent) QueryOne(field string, where interface{}, args ...interface{}) error {
+	mdb := db.GetMysqlDB()
+	err := mdb.Model(m).Select(field).Where(where, args...).Last(m).Error
+	if err != nil {
+		util.LoggerError(err)
+		return err
+	}
+
+	return nil
+}
+
+func (m *StoryContent) QueryCount(where interface{}, args ...interface{}) (int, error) {
+	mdb := db.GetMysqlDB()
+	var count int
+	err := mdb.Model(m).Where(where, args...).Count(&count).Error
+	if err != nil {
+		util.LoggerError(err)
+		return 0, err
+	}
+	return count, nil
+}
+
+func (m *StoryContent) QueryGrounp(field string, group string, where interface{}, args ...interface{}) ([]StoryContent, error) {
+	mdb := db.GetMysqlDB()
+	var list = make([]StoryContent, 0)
+	err := mdb.Model(m).Select(field).Where(where, args...).Group(group).Find(&list).Error
+	if err != nil {
+		util.LoggerError(err)
+		return make([]StoryContent, 0), err
+	}
+	return list, nil
 }
